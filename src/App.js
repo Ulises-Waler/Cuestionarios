bdab0268212d941ad795aa5710b8aa0066b4ed52
Ulises Vargas <utm23090479@utma.edu.mx>
2024-09-27 03:34:22 -0600

    Se agregó el código de recuperar contraseña

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import { Container, Card, } from "react-bootstrap";
 
 const App = () => {
-  const style = {
-    backgroundColor: 'lightblue',
-  };
+  
   return (
-    <center><div style={style}><Container className="mt-5">
-      <Card style={{ width: '18rem' }}>
-        <Card.Body>
-          <Card.Title>Cuestionarios</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">Creacion de Cuestionarios dinámicos</Card.Subtitle>
-          <Card.Text>
-            Se requiere una plataforma que cree cuestionarios de manera dinámica. Que el usuario pueda crear preguntas y asignarle un tipo,si va a recibir datos númericos, de texto, opcionales, etc, etc...La plataforma también debe de guardar el dueño de dicho cuestionario y mostrar en gráfica la respuestas que reciba.
-          </Card.Text>
-          <Card.Link href="#">Brazilian Miku</Card.Link>
-          <Card.Link href="#">Yuta Okkotsu</Card.Link>
-        </Card.Body>
-      </Card>
-    </Container>
-    </div></center>
+    <>
+    </>
   );
 }
 
