c1e1d542f3ff6c3c287cd7e795a87a16b34489f0
Angel-padill <utm23090741@utma.edu.mx>
2024-09-29 00:39:54 -0600

    ya subi lo mio

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -9,6 +13,18 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <App/>
+  },
+  {
+    path: "/login",
+    element: <Login/>
+  },
+  {
+    path: "/recover-password",
+    element: <Recover-password/>
+  },
+  {
+    path: "/home",
+    element: <Home/>
   },
   {
     path: "/recover-password",
